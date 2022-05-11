77b094c69908ced8296c11beb31333658471e0c3
WY <s1cca@outlook.com>
2022-05-11 11:41:50 +0000

    testing files updated

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
 from application import app, db
-from application.model import Character
+# from application.model import Character
 import requests, json
 
 @app.route('/', methods=['GET', 'POST'])
--- service_3/tests/test_service_3.py
+++ service_3/tests/test_service_3.py
@@ -1,6 +1,6 @@
 from flask import url_for
 from flask_testing import TestCase
-from application import app, gender
+from service_3.app import app, gender
 
 class TestBase(TestCase):
     def create_app(self):
