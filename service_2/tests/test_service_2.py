db4cc71801f992b497d112c2f5a72df06d4c88fb
WY <s1cca@outlook.com>
2022-05-11 11:50:25 +0000

    test file updated

--- service_2/tests/test_service_2.py
+++ service_2/tests/test_service_2.py
@@ -1,7 +1,7 @@
 from flask import url_for
 from flask_testing import TestCase
 
-from application import app, classes
+from app import app, classes
 
 class TestBase(TestCase):
     def create_app(self):
