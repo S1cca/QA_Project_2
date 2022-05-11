269921696918031acc5c74e8bcd6291eda66ecfa
WY <s1cca@outlook.com>
2022-05-11 11:47:08 +0000

    deleted sql stuff

--- service_1/application/model.py
+++
@@ -1,9 +0,0 @@
-from application import db
-
-class Character(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    classes = db.Column(db.String(80), nullable=False)   # Service 2
-    gender = db.Column(db.String(80), nullable=False)    # Service 3
-    subclass = db.Column(db.String(80), nullable=False)  # Service 4
-    birth_place = db.Column(db.String(80), nullable=False) # Service 4
-    
