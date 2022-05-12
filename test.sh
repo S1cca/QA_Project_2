12fccf1c211de74e852038e8692bc8c584156f90
WY <s1cca@outlook.com>
2022-05-12 09:07:35 +0000

    added install wheel

--- test.sh
+++ test.sh
@@ -4,11 +4,12 @@ for dir in "${directories[@]}"
 do
     cd ${dir}
     sudo apt-get update
-    sudo apt-get install python3 python3-pip python3-venv
+    sudo apt-get install python3 python3-pip python3-venv python3-wheel -y
+    pip3 install wheel
     python3 -m venv venv
     source venv/bin/activate
     pip3 install -r requirements.txt
-    python3 -m pytest --cov=. 
+    python3 -m pytest --cov=.
     deactivate
     cd ..
 done
