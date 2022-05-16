fabd53d24da7da0a2c4314b26a7405463b8eeadf
WY <s1cca@outlook.com>
2022-05-16 10:04:27 +0000

    added some space to make code look nice

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -8,12 +8,11 @@ def home():
     classes = requests.get('http://service_2:5000/get_classes').text
     gender = requests.get('http://service_3:5000/get_gender').text
     character = {'classes': classes, 'gender': gender}
-    status = requests.post('http://service_4:5000/post_status', json=character).json()
+    status = requests.post('http://service_4:5000/post_status', json = character).json()
 
-    return render_template('home.html',classes=classes, gender=gender, status = status )
-
-
+    return render_template('home.html', classes = classes, gender = gender, status = status )
 
 
 
-    
+
+
