337344ad63e9ff0ce6daa5d22f61d201de4c1060
Wei Yao <76656869+S1cca@users.noreply.github.com>
2022-05-18 12:23:33 +0100

    Update __init__.py

--- service_1/application/__init__.py
+++ service_1/application/__init__.py
@@ -4,9 +4,4 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@35.242.133.231:3306/project_2"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-
-db = SQLAlchemy(app)
-
 from application import routes
