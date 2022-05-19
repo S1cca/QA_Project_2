d5a21bfb3a2aacb67bc5b6800467d42aaa535d7c
Wei Yao <76656869+S1cca@users.noreply.github.com>
2022-05-19 22:11:54 +0100

    Update routes.py
    delete db

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
-from application import app, db
+from application import app
 import requests, json
 
 @app.route('/', methods=['GET', 'POST'])
